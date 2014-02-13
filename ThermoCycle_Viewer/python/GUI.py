eac301504cf1696d796b435f2cb07995c384bc9d
Ian bell <ian.h.bell@gmail.com>
2014-02-13 12:17:38 +0100

    Added code for screenshot/animation
    Signed-off-by: Ian bell <ian.h.bell@gmail.com>

--- ThermoCycle_Viewer/python/GUI.py
+++ ThermoCycle_Viewer/python/GUI.py
@@ -297,6 +297,10 @@ class MainFrame(wx.Frame):
         # Re-plot
         self.OnChangeStep()
         
+        # Uncomment this line to take a screenshot of the entire GUI window
+        # This can be used to save an animation
+        #self.TakeScreenShot()
+        
     def OnRefresh(self, event = None):
         
         self.OnBackgroundPlot()
@@ -370,7 +374,6 @@ class MainFrame(wx.Frame):
             pmin,pmax = max(pmin-0.05*(pmax-pmin),0),max(CP.PropsSI(self.fluid,'pcrit')*1.05,pmax)
             ax.set_ylim(pmin, pmax)
             ax.set_yscale('log')
-                        
         
         ax = self.T_profile_plot.ax
         
@@ -534,6 +537,58 @@ class MainFrame(wx.Frame):
             if hasattr(self,'PT'):
                 self.PT.shutdown()
                 del self.PT
+                
+    def TakeScreenShot(self):
+        """ Takes a screenshot of the screen at give pos & size (rect). """
+        rect = self.GetRect()
+        # see http://aspn.activestate.com/ASPN/Mail/Message/wxpython-users/3575899
+        # created by Andrea Gavana
+        time.sleep(0.1)
+
+        # adjust widths for Linux (figured out by John Torres 
+        # http://article.gmane.org/gmane.comp.python.wxpython/67327)
+        if sys.platform == 'linux2':
+            client_x, client_y = self.ClientToScreen((0, 0))
+            border_width = client_x - rect.x
+            title_bar_height = client_y - rect.y
+            rect.width += (border_width * 2)
+            rect.height += title_bar_height + border_width
+ 
+        #Create a DC for the whole screen area
+        dcScreen = wx.ScreenDC()
+ 
+        #Create a Bitmap that will hold the screenshot image later on
+        #Note that the Bitmap must have a size big enough to hold the screenshot
+        #-1 means using the current default colour depth
+        bmp = wx.EmptyBitmap(rect.width, rect.height)
+ 
+        #Create a memory DC that will be used for actually taking the screenshot
+        memDC = wx.MemoryDC()
+ 
+        #Tell the memory DC to use our Bitmap
+        #all drawing action on the memory DC will go to the Bitmap now
+        memDC.SelectObject(bmp)
+ 
+        #Blit (in this case copy) the actual screen on the memory DC
+        #and thus the Bitmap
+        memDC.Blit( 0, #Copy to this X coordinate
+                    0, #Copy to this Y coordinate
+                    rect.width, #Copy this width
+                    rect.height, #Copy this height
+                    dcScreen, #From where do we copy?
+                    rect.x, #What's the X offset in the original DC?
+                    rect.y  #What's the Y offset in the original DC?
+                    )
+ 
+        #Select the Bitmap out of the memory DC by selecting a new
+        #uninitialized Bitmap
+        memDC.SelectObject(wx.NullBitmap)
+ 
+        img = bmp.ConvertToImage()
+        
+        # Get the slider value
+        fileName = 'frame{i:04d}.png'.format(i=self.bottompanel.step.GetValue())
+        img.SaveFile(fileName, wx.BITMAP_TYPE_PNG)
                           
 if __name__ == '__main__':
     # The following line is required to allow cx_Freeze 
